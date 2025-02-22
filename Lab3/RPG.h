1b8e721b98fa6f782a8b176414d877a8d9a0f7c2
kilorecon <nathanbernal7@gmail.com>
2025-02-22 12:04:08 -0800

    Update RPG.h

--- Lab3/RPG.h
+++ Lab3/RPG.h
@@ -1,1 +1,38 @@
+#ifndef RPG_H
+#define RPH_H
+#include <string>
 
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG {
+    public:
+    //--------Constructors--------//
+    RPG();
+    RPG(string name, int health, int strength, int defense, string type);
+
+    //--------Mutators--------//
+    void setSkills();
+    void printAction(string, RPG);
+    void updateHealth(int);
+    //void attack(RPG *);
+    //void useSkill(RPG *);
+
+    //--------Accessors--------//
+    bool isAlive() const;
+    string getName() const;
+    int getHealth() const;
+    int getStrength() const;
+    int getDefense() const;
+
+
+    private:
+    string name;
+    int health;
+    int strength;
+    int defense;
+    string type;
+    string skills[SKILL_SIZE];
+};
+#endif
