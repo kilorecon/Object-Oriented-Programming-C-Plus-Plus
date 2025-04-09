cc46ffe82ed804e67017180c4a18379aff29adaa
kilorecon <nathanbernal7@gmail.com>
2025-04-09 15:16:29 -0700

    Update Charmander.h

--- Lab5/Charmander.h
+++ Lab5/Charmander.h
@@ -1,1 +1,23 @@
+#ifndef CHARMANDER_H
+#define CHARMANDER_H
 
+#include "Pokemon.h"
+
+using namespace std;
+
+class Charmander : public Pokemon {
+    public:
+        //------------------Constructors------------------//
+        Charmander();
+        Charmander(string name, int hp, int att, int def, vector<string> t, vector<string> s);
+
+        //------------------Mutators------------------//
+        void speak(); /*override*/
+        void printStats(); /*override*/
+
+    private:
+        vector<string> skills;
+        // name, hp, attack, defense inherited from Pokemon base class //
+
+};
+#endif
