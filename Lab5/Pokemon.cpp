19b64eb0be70480e2c7c188dad4ee2c8f7e34279
kilorecon <nathanbernal7@gmail.com>
2025-04-09 15:16:58 -0700

    Update Pokemon.cpp

--- Lab5/Pokemon.cpp
+++ Lab5/Pokemon.cpp
@@ -1,1 +1,33 @@
+#include "Pokemon.h"
+#include <iostream>
 
+
+Pokemon::Pokemon() {
+    name = "unidentified";
+    hp = 0;
+    attack = 0;
+    defense = 0;
+    cout << "Defense Constructor (Pokemon)\n";
+}
+
+Pokemon::Pokemon(string name, int hp, int att, int def, vector<string> type) {
+    this->name = name;
+    this->hp = hp;
+    attack = att;
+    defense = def;
+    type = type;
+    cout << "Overloaded Constructor (Pokemon)\n";
+}
+
+void Pokemon::speak() {
+    cout << "...\n";
+}
+
+void Pokemon::printStats() {
+    printf("Name: %s\t HP: %i \tDEF: %i\t ATT: %i\n", name.c_str(), hp, defense, attack);
+    cout << "Type: ";
+    for (int i = 0; i < type.size(); i++) {
+        cout << type[i] << "\t";
+    }
+    cout << endl;
+}
