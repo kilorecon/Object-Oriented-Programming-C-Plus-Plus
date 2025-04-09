a6d5946b64eed842a124e561a14f2c864628c19a
kilorecon <nathanbernal7@gmail.com>
2025-04-09 15:17:11 -0700

    Update Pokemon.h

--- Lab5/Pokemon.h
+++ Lab5/Pokemon.h
@@ -1,1 +1,29 @@
+#ifndef POKEMON_H
+#define POKEMON_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
+class Pokemon {
+    public:
+        //------------------Constructors------------------//
+        Pokemon();
+        Pokemon(string name, int hp, int att, int def, vector<string> type);
+
+        //------------------Mutators------------------//
+        virtual void speak();
+        virtual void printStats();
+
+        //------------------Accessors------------------//
+
+    
+    protected:
+    string name;
+    int hp;
+    int attack;
+    int defense;
+    vector<string> type;
+    
+};
+#endif
